46c315cdf0b41dec1bc883021c57df27e4289b6d
Marina Olneva <112516911+Marinchk@users.noreply.github.com>
2024-06-03 00:32:05 +0200

    fix
    some fix

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,56 +1,50 @@
 const path = require('path');
 const Project = require('../models/Project');
 
-const projectList = [];
 
 exports.getProjects = (_, res) => {
-    res.render('list', { projectList });
-};
+    res.render('list', { projectList: Project.projectList });
+}
 
 exports.addProject = (_, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'addProject.html'));
 };
 
 exports.newProject = (req, res) => {
-    const { name, description, users } = req.body;
-    projectList.push(new Project(name, description, users));
-    res.redirect('/');
+    try {
+        Project.addProject(new Project(req.body.name, req.body.description, req.body.users));
+        res.redirect('/');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 };
 
 exports.getUpdateProject = (req, res) => {
     const id = req.params.id;
-    if (!projectList[id]) {
-        res.status(404).send('No project');
-    } else {
-        const project = projectList[id];
+    const project = Project.findById(id);
+    if (project) {
         project.id = id;
         res.render('updateProject', { project });
+    } else {
+        res.status(404).send('No project found');
     }
 };
-
 
 exports.updateProject = (req, res) => {
     const id = req.params.id;
-    const { name, description, users } = req.body;
-    if (!projectList[id]) {
-        res.status(404).send('No project');
-    } else {
-        projectList[id].name = name;
-        projectList[id].description = description;
-        projectList[id].users = users;
+    try {
+        Project.updateProject(id, req.body.name, req.body.description, req.body.users);
         res.redirect('/');
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 };
 
-
-
-
 exports.deleteProject = (req, res) => {
-    const id = req.params.id;
-    if (!projectList[id]) {
-        res.status(404).send('No project');
-    } else {
-        projectList.splice(id, 1);
+    try {
+        Project.deleteProject(req.params.id);
         res.redirect('/');
+    } catch (error) {
+    res.status(404).send(error.message);
     }
 };
--- models/Project.js
+++ models/Project.js
@@ -1,8 +1,50 @@
 class Project {
+  static projectList = [];
+
   constructor(name, description, users) {
-    this.name = name;
-    this.description = description;
-    this.users = users;
+      this.name = name;
+      this.description = description;
+      this.users = users;
+  }
+
+  static validate(project) {
+      const isNameValid = project.name.trim().length > 0;
+      const isDescriptionValid = project.description.length > 10;
+      return isNameValid && isDescriptionValid;
+  }
+
+  static addProject(project) {
+      if (this.validate(project)) {
+          this.projectList.push(project);
+          return project;
+      } else {
+          throw new Error("Invalid project data");
+      }
+  }
+
+  static findById(id) {
+      return this.projectList[id];
+  }
+
+  static updateProject(id, name, description, users) {
+      let project = this.findById(id);
+      if (project && this.validate(new Project(name, description, users))) {
+          project.name = name;
+          project.description = description;
+          project.users = users;
+          return project;
+      } else {
+          throw new Error("Invalid project data or project not found");
+      }
+  }
+
+  static deleteProject(id) {
+      if (this.findById(id)) {
+          this.projectList.splice(id, 1);
+          return true;
+      } else {
+          throw new Error("Project not found");
+      }
   }
 }
 
